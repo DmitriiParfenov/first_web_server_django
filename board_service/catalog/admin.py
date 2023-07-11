4d205af5d637fa91a78beceeda86e84756e091b1
Dmitrii <dima12sik@yandex.ru>
2023-07-11 13:21:29 +0300

    updated admin.py to register product and category databases

--- board_service/catalog/admin.py
+++ board_service/catalog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Product, Category
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'published', "changed")
+    list_display_links = ('name', 'category', 'price', 'published', )
+    list_filter = ('category', )
+    search_fields = ('name', 'description', )
+
+
+@admin.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', )
+    list_display_links = ('title', )
