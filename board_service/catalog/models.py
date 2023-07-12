b6a829d518e6bdbb81f4eac40300897f319a1c11
Dmitrii <dima12sik@yandex.ru>
2023-07-12 13:26:57 +0300

    add Feedback models

--- board_service/catalog/models.py
+++ board_service/catalog/models.py
@@ -5,6 +5,12 @@ NULLABLE = {'blank': True, 'null': True}
 
 # Create your models here.
 class Product(models.Model):
+    """
+    Модель Product для сохранения определенного товара в базе данных. Поля:
+    1) name — название товара, 2) description — описание товара, 3) image — иконка товара, 4) published — дата
+    публикации, 5) changed — дата изменения, 6) price — цена, 7) category — категория, ссылающаяся на поле модели
+    Category.
+    """
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(**NULLABLE, verbose_name='Описание')
     image = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Изображение')
@@ -23,13 +29,51 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=50, verbose_name='Категория')
+    """
+    Модель Category для сохранения категорий товаров в базе данных. Поля:
+    1) category — название категории, 2) description — описание категории.
+    """
+    category = models.CharField(max_length=50, verbose_name='Категория')
     description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
-        return f'{self.title}'
+        return f'{self.category}'
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-        ordering = ['title']
+        ordering = ['category']
+
+
+class Feedback(models.Model):
+    """
+    Модель Feedback для обратной связи от пользователей в базе данных. Поля:
+    1) first_name — имя пользователя, 2) last_name — фамилия пользователя, 3) email — электронная почта пользователя,
+    4) phone — номер телефона пользователя, 5) country — страна, откуда пользователь, 6) message — сообщение от
+    пользователя, 7) publish — дата публикации.
+    """
+    first_name = models.CharField(max_length=100, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+    email = models.EmailField(max_length=100, verbose_name='Электронная почта')
+    phone = models.CharField(max_length=30, verbose_name='Номер телефона', **NULLABLE)
+
+    # Страна выбирается из выпадающего списка
+    class Kinds(models.TextChoices):
+        RUSSIA = 'Россия', 'Россия'
+        AZERBAIJAN = 'Азербайджан', 'Азербайджан'
+        ARMENIA = 'Армения', 'Армения'
+        BELARUS = 'Белоруссия', 'Белоруссия'
+        KAZAKHSTAN = 'Казахстан', 'Казахстан'
+        KYRGYZSTAN = 'Киргизия', 'Киргизия'
+        MOLDAVIA = 'Молдавия', 'Молдавия'
+        TAJIKISTAN = 'Таджикистан', 'Таджикистан'
+        UZBEKISTAN = 'Узбекистан', 'Узбекистан'
+
+    country = models.CharField(choices=Kinds.choices, default=Kinds.RUSSIA, verbose_name='Страна')
+    message = models.TextField(verbose_name='Сообщение')
+    publish = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
+
+    class Meta:
+        verbose_name = 'Обратная связь'
+        verbose_name_plural = 'Обратные связи'
+        ordering = ['-publish']
