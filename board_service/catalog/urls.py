98e2d8acaee26a9be649fa2601c174550f1ba271
Dmitrii <dima12sik@yandex.ru>
2023-07-12 13:27:24 +0300

    updated urls.py

--- board_service/catalog/urls.py
+++ board_service/catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index, feedback
+from .views import index, feedback, get_products, get_products_by_word, post_feedback
 
 
 urlpatterns = [
     path('', index, name='main'),
     path('feedback/', feedback, name='feedback'),
+    path('products/', get_products, name='products'),
+    path('products/<str:keyword>/', get_products_by_word, name='keyword'),
+    path('about/', post_feedback, name='about')
 ]
