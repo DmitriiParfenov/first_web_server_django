5e1a0a158bbad30fffc2afdbb99dcd1d2b33da70
Dmitrii <dima12sik@yandex.ru>
2023-07-14 15:54:38 +0300

    add pagination tool for get_products controller

--- board_service/catalog/models.py
+++ board_service/catalog/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-        ordering = ['-published']
+        ordering = ('-published', )
 
 
 class Category(models.Model):
