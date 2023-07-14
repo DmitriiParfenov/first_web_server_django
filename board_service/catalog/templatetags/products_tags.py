38c22f6bd9d2ef58cb80b76e8b3d0a602d745268
Dmitrii <dima12sik@yandex.ru>
2023-07-14 15:55:05 +0300

    updated product_tags.py

--- board_service/catalog/templatetags/products_tags.py
+++ board_service/catalog/templatetags/products_tags.py
@@ -1,4 +1,5 @@
 from django import template
+
 from catalog.models import Product
 
 register = template.Library()
@@ -9,4 +10,11 @@ def product_by_keyword(keyword):
     user_object = Product.objects.filter(name__icontains=keyword)
     if user_object.exists():
         return user_object
+    return False
+
+
+@register.filter()
+def get_image_for_product(product):
+    if product:
+        return f'media/{product}'
     return False
